24a99f61266f76912689945a229a755042509646
Amrani005 <superamrani@gmail.com>
2026-06-10 23:26:44 +0100

    nextauth url

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -5,7 +5,7 @@ import Header from '@/components/Header'
 
 const page = () => {
   return (
-    <div className='flex flex-col gap-40'>
+    <div className='flex flex-col gap-"0'>
       <Header/>
       <Cart/>
       <Footer/>
